cbe236fa369b7c71d622ce8abb1d502542a8a8ab
Tom K <tom.kain@gmail.com>
2022-07-09 02:56:32 +0000

    Add rstudio docker image

--- sh/docker_init.sh
+++ sh/docker_init.sh
@@ -1,4 +1,11 @@
+!#/bin/bash
+
 docker run -d \
 -p 4445:4444 -p 5901:5900 \
 --memory 4096mb --shm-size 4g \
 selenium/standalone-firefox-debug:latest
+
+docker run --name tk_rstudio \
+-e PASSWORD=password -e ROOT=TRUE \
+-d -v {$PWD}:/var/repos \
+-p 8787:8787 rocker/tidyverse
